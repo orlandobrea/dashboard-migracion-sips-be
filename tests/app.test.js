aac20fc1637e3ee25c491799491bec1bd06ea4cc
Orlando Brea <orlando.brea@gmail.com>
2021-02-24 12:01:51 -0300

    feature: agrego testeo

---
+++ tests/app.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../server');
+const { version } = require('../package.json');
+
+describe('Endpoints', () => {
+  it('Check /health', async () => {
+    const res = await request(app).get('/health');
+    expect(res.statusCode).toBe(200);
+  });
+  it('Check /version', async () => {
+    const res = await request(app).get('/version');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('version');
+    expect(res.body.version).toBe(version);
+  });
+});
