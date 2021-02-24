91d62d8f97525991f2332632c5b306aff4a3abb7
Orlando Brea <orlando.brea@gmail.com>
2021-02-24 12:31:58 -0300

    fix: corrijo errores introducidos en testeo (#5)

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const app = require('./server')
+const {app, connect} = require('./server')
+
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 
 const server = app.listen(PORT, () => {
   connect();
   console.log(`App running on port ${PORT}`);
 });
 
-module.exports = {
-    server
-}
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const { app } = require('../server');
 const { version } = require('../package.json');
 
 describe('Endpoints', () => {
