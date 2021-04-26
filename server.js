df3448bf54ead3823a919b4d04a6a2a416aca055
Orlando Brea <orlando.brea@gmail.com>
2021-04-26 13:01:19 -0300

    chore: format code

--- server.js
+++ server.js
@@ -2,81 +2,81 @@ const app = require('express')();
 const cors = require('cors');
 const mssql = require('mssql');
 const dotenv = require('dotenv');
-const { version } = require('./package.json');
+const {version} = require('./package.json');
 const moment = require('moment');
 
 dotenv.config();
 
 app.use(
-  cors({
-    methods: ['GET', 'OPTIONS'],
-  }),
+    cors({
+        methods: ['GET', 'OPTIONS'],
+    }),
 );
 
 let connectionStatus = 'ok';
 
-mssql.on('error', (_) => (connectionStatus = 'error'));
+mssql.on('error', _ => (connectionStatus = 'error'));
 
 const connect = async () => {
-  try {
-    await mssql.connect(
-      `mssql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_DATABASE}`,
-    );
-    connectionStatus = 'ok';
-  } catch (err) {
-    connectionStatus = 'error';
-    console.log(err);
-    setTimeout(() => connect(), 5000);
-  }
+    try {
+        await mssql.connect(
+            `mssql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_DATABASE}`,
+        );
+        connectionStatus = 'ok';
+    } catch (err) {
+        connectionStatus = 'error';
+        console.log(err);
+        setTimeout(() => connect(), 5000);
+    }
 };
 
 app.get('/api/version', (req, res) => {
-  res.json({
-    version: version,
-  });
+    res.json({
+        version: version,
+    });
 });
 
-app.get('/api/health', (req, res) => {
-  res.json({
-    status: {
-      app: 'ok',
-      db: connectionStatus,
-    },
-  });
+    res.json({
+        app.get('/api/health', (req, res) => {
+        status: {
+            app: 'ok',
+            db: connectionStatus,
+        },
+    });
 });
 
 app.get('/api/unhealthy_endpoint', (req, res) => {
-  const ok = Math.random() > 0 ? true : false;
-  if (!ok) {
-    res.status(500).json({ status: 'error' });
-  } else {
-    res.json({
-      status: 'ok',
-    });
-  }
+    const ok = Math.random() > 0 ? true : false;
+    if (!ok) {
+        res.status(500).json({status: 'error'});
+    } else {
+        res.json({
+            status: 'ok',
+        });
+    }
 });
 
 app.get('/api', async (req, res) => {
-  try {
-    const formatDate = (data) => moment.utc(data).utcOffset('-0300', true);
-    const query = await mssql.query(
-      `select lesg.* 
+    try {
+        const formatDate = data => moment.utc(data).utcOffset('-0300', true);
+        const query = await mssql.query(
+            `select lesg.* 
       from SIPS.dbo.LAB_EstadoSyncGeneral lesg WITH (NOLOCK) `,
-    );
-    const response = query.recordsets[0].map((row) => ({
-      ...row,
-      ultimoSyncFechaInicio: formatDate(row.ultimoSyncFechaInicio),
-      ultimoSyncFechaFin: formatDate(row.ultimoSyncFechaFin),
-      ultimoUpdateEfectorInicio: formatDate(row.ultimoUpdateEfectorInicio),
-      ultimoUpdateEfectorFin: formatDate(row.ultimoUpdateEfectorFin),
-    }));
-    res.send(response);
-  } catch (e) {
-    res.status(500).json(e);
-  }
+        );
+        const response = query.recordsets[0].map(row => ({
+            ...row,
+            ultimoSyncFechaInicio: formatDate(row.ultimoSyncFechaInicio),
+            ultimoSyncFechaFin: formatDate(row.ultimoSyncFechaFin),
+            ultimoUpdateEfectorInicio: formatDate(row.ultimoUpdateEfectorInicio),
+            ultimoUpdateEfectorFin: formatDate(row.ultimoUpdateEfectorFin),
+        }));
+        res.send(response);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
 
 module.exports = {
-  app,
-  connect,
+    app,
+    connect,
 };
